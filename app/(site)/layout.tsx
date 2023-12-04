e636dc6c96ff97549dfda27fdf5b3c27b7946345
Azizur Rahman <azizurrahmanemon007@gmail.com>
2023-12-04 21:14:23 +0600

    final

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,6 +1,6 @@
 // app/layout.tsx
 
-import "./globals.css";
+import "../globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "./components/global/Navbar";
